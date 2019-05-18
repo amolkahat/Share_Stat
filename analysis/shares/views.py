6076f8f4d3ccc0503e7cbd4719bd3e1185ca68bd
Amol Kahat <akahat@redhat.com>
2019-05-18 15:26:17 +0530

    Added login form working example.
    Signed-off-by: Amol Kahat <akahat@redhat.com>

--- analysis/shares/views.py
+++ analysis/shares/views.py
@@ -6,6 +6,18 @@ def index(request):
     return render(request, "pages/home.html")
 
 def login(request):
+    if request.method == 'POST':
+        userid = ''
+        password = ''
+        try:
+            userid = request.POST['uname1']
+            password = request.POST['pwd1']
+            print(userid, password)
+        except:
+            print("Error..")
+
+        if userid == 'janedone' and password == 'Secret123':
+            return render(request, 'pages/home.html')
     return render(request, "pages/login.html")
 
 def signup(request):
