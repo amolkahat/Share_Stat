3dd9445b4535de4c1add818f9a48262167313c84
Amol Kahat <akahat@redhat.com>
2019-05-18 15:07:43 +0530

    Html pages optimization using jinja tempating
    Signed-off-by: Amol Kahat <akahat@redhat.com>

--- analysis/shares/views.py
+++ analysis/shares/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, "pages/index.html")
+    return render(request, "pages/home.html")
 
 def login(request):
     return render(request, "pages/login.html")
