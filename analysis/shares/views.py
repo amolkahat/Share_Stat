0e3b7a0cd1809d7c4166a6d93ed809b2f09a4b3a
Amol Kahat <akahat@redhat.com>
2019-05-18 14:49:59 +0530

    Added urls, views for the app.
    Signed-off-by: Amol Kahat <akahat@redhat.com>

--- analysis/shares/views.py
+++ analysis/shares/views.py
@@ -3,4 +3,16 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("HEllo WOrld This is shares app")
+    return render(request, "pages/index.html")
+
+def login(request):
+    return render(request, "pages/login.html")
+
+def signup(request):
+    return render(request, "pages/signup.html")
+
+def about(request):
+    return render(request, "pages/about.html")
+
+def contact(request):
+    return render(request, "pages/contact.html")
